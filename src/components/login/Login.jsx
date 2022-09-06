62a5460d1cc5ae21f7a48ae044b3e9058e93ff90
Farul Ivan <farulivan@gmail.com>
2022-09-06 19:54:12 +0700

    finishing the tutorial

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,12 +1,42 @@
 // in this course we try to use TDD (Test-driven dev), which we create the test first before development
 
+import { useState } from "react"
+import axios from "axios"
+
 const Login = () => {
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [user, setUser] = useState({})
+
+    const handleClick = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+        try{
+            const {data} = await axios.get(
+                "https://jsonplaceholder.typicode.com/users/1"
+            )
+            setUser(data)
+        } catch {
+            setError(true)
+        }
+        setLoading(false)
+    }
+
     return (
         <div className="container">
+            <span className="user">{user.name}</span>
             <form>
-                <input type="text" />
-                <input type="password" />
-                <button>Login</button>
+                <input type="text" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <button 
+                    disabled={!username || !password} 
+                    onClick={handleClick}
+                >
+                    {loading? "please wait.." : "Login"}
+                </button>
+                <span data-testid="error" style={{visibility: error ? "visible" : "hidden"}}>Something went wrong</span>
             </form>
         </div>
     )
--- src/components/login/Login.test.js
+++ src/components/login/Login.test.js
@@ -1,10 +1,21 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen, waitFor } from "@testing-library/react"
 import Login from "./Login"
+
+jest.mock("axios", () => ({
+    
+    __esModule:true,
+
+    default:{
+        get: ()=> ({
+            data:{id:1, name:"John"}
+        })
+    }
+}))
 
 test("username input should be rendered", () => {
     render(<Login />)
-    const userInputEl = screen.getByPlaceholderText(/username/i)
-    expect(userInputEl).toBeInTheDocument()
+    const usernameInputEl = screen.getByPlaceholderText(/username/i)
+    expect(usernameInputEl).toBeInTheDocument()
 })
 
 test("password input should be rendered", () => {
@@ -17,4 +28,114 @@ test("login button should be rendered", () => {
     render(<Login />)
     const loginButton = screen.getByRole("button")
     expect(loginButton).toBeInTheDocument()
+})
+
+test("username input should be empty", () => {
+    render(<Login />)
+    const usernameInputEl = screen.getByPlaceholderText(/username/i)
+    expect(usernameInputEl.value).toBe("")
+})
+
+test("password input should be empty", () => {
+    render(<Login />)
+    const passwordInputEl = screen.getByPlaceholderText(/password/i)
+    expect(passwordInputEl.value).toBe("")
+})
+
+test("login button should be disabled", () => {
+    render(<Login />)
+    const loginButton = screen.getByRole("button")
+    expect(loginButton).toBeDisabled()
+})
+
+test("loading button should not be rendered", () => {
+    render(<Login />)
+    const loginButton = screen.getByRole("button")
+    expect(loginButton).not.toHaveTextContent(/please wait/i)
+})
+
+test("error message should not be visible", () => {
+    render(<Login />)
+    const errorEl = screen.getByTestId("error")
+    expect(errorEl).not.toBeVisible()
+})
+
+test("username input should change", () => {
+    render(<Login />)
+    const usernameInputEl = screen.getByPlaceholderText(/username/i)
+    const testValue = "test"
+
+    fireEvent.change(usernameInputEl, { target: { value: testValue } })
+    expect(usernameInputEl.value).toBe(testValue)
+})
+
+test("password input should change", () => {
+    render(<Login />)
+    const passwordInputEl = screen.getByPlaceholderText(/password/i)
+    const testValue = "test"
+    
+    fireEvent.change(passwordInputEl, { target: { value: testValue } })
+    expect(passwordInputEl.value).toBe(testValue)
+})
+
+test("login button should not be disabled when inputs exist", () => {
+    render(<Login />)
+    const loginButton = screen.getByRole("button")
+    const usernameInputEl = screen.getByPlaceholderText(/username/i)
+    const passwordInputEl = screen.getByPlaceholderText(/password/i)
+
+    const testValue = "test"
+    fireEvent.change(usernameInputEl, { target: { value: testValue } })
+    fireEvent.change(passwordInputEl, { target: { value: testValue } })
+
+    expect(loginButton).not.toBeDisabled()
+})
+
+test("loading button should be rendered when click", () => {
+    render(<Login />)
+    const loginButton = screen.getByRole("button")
+    const usernameInputEl = screen.getByPlaceholderText(/username/i)
+    const passwordInputEl = screen.getByPlaceholderText(/password/i)
+
+    const testValue = "test"
+
+    fireEvent.change(usernameInputEl, { target: { value: testValue } })
+    fireEvent.change(passwordInputEl, { target: { value: testValue } })
+    fireEvent.click(loginButton)
+
+    expect(loginButton).toHaveTextContent(/please wait/i)
+})
+
+test("loading button should not be rendered after fetching", async () => {
+    render(<Login />)
+    const loginButton = screen.getByRole("button")
+    const usernameInputEl = screen.getByPlaceholderText(/username/i)
+    const passwordInputEl = screen.getByPlaceholderText(/password/i)
+
+    const testValue = "test"
+
+    fireEvent.change(usernameInputEl, { target: { value: testValue } })
+    fireEvent.change(passwordInputEl, { target: { value: testValue } })
+    fireEvent.click(loginButton)
+
+    await waitFor(() => 
+         expect(loginButton).not.toHaveTextContent(/please wait/i)
+    ) 
+})
+
+test("user should be rendered after fetching", async () => {
+    render(<Login />)
+    const loginButton = screen.getByRole("button")
+    const usernameInputEl = screen.getByPlaceholderText(/username/i)
+    const passwordInputEl = screen.getByPlaceholderText(/password/i)
+
+    const testValue = "test"
+
+    fireEvent.change(usernameInputEl, { target: { value: testValue } })
+    fireEvent.change(passwordInputEl, { target: { value: testValue } })
+    fireEvent.click(loginButton)
+
+    const userItem = await screen.findByText("John")
+
+    expect(userItem).toBeInTheDocument()
 })
